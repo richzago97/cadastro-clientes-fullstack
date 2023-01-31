afc7204716576e56b60c46283ccee23647b42925
richzago <rich.czago@gmail.com>
2023-01-31 20:04:26 -0300

    feat:Connection established with PostgreSQL database

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -1,24 +1,24 @@
 import { DataSource } from "typeorm";
+import "dotenv/config";
 
-require('dotenv').config()
-
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: false,
-    logging: true,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"],
-})
-
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source initialized")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
+export const AppDataSource = new DataSource(
+  process.env.NODE_ENV === "test"
+    ? {
+        type: "sqlite",
+        database: ":memory:",
+        synchronize: true,
+        entities: ["src/entities/*.ts"],
+      }
+    : {
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "rzago",
+        password: "123456",
+        database: "fullstack_form_1",
+        logging: true,
+        synchronize: false,
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
+      }
+);
