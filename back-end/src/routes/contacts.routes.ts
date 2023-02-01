fcfb67e2773ebcf391c91ab4ece270d2d6613f34
richzago <rich.czago@gmail.com>
2023-02-01 11:06:11 -0300

    feat: added import list contact controller

--- back-end/src/routes/contacts.routes.ts
+++ back-end/src/routes/contacts.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { listClientController } from "../controllers/clients/client.controller";
-import { createContactController } from "../controllers/contacts/contact.controller";
+import {
+  createContactController,
+  listContactController,
+} from "../controllers/contacts/contact.controller";
 import { authClient } from "../middlewares/authClient.middleware";
 
 const contactsRouter = Router();
 
 contactsRouter.post("", authClient, createContactController);
-contactsRouter.get("", authClient, listClientController);
+contactsRouter.get("", authClient, listContactController);
 
 export default contactsRouter;
