a5301d20707bb4465bbaf89a968bf285716b4f7c
richzago <rich.czago@gmail.com>
2023-02-02 15:49:05 -0300

    fix: added env

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import { AppDataSource } from "./data-source";
 import "dotenv/config";
 
+
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
