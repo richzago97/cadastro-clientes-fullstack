f586cc8e0706a79bca067542cda7511b0dfb4ff6
richzago <rich.czago@gmail.com>
2023-02-02 16:11:13 -0300

    feat: created migrations

--- back-end/src/migrations/1675365045860-initialMigration.ts
+++ back-end/src/migrations/1675365045860-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1675342782392 implements MigrationInterface {
+export class initialMigration1675365045860 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
