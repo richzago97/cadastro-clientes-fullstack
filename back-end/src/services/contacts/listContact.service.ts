0546050895a285d4cab73462e23e013617609525
richzago <rich.czago@gmail.com>
2023-02-02 10:17:17 -0300

    fix: now with relations

--- back-end/src/services/contacts/listContact.service.ts
+++ back-end/src/services/contacts/listContact.service.ts
@@ -1,25 +1,14 @@
 import { AppDataSource } from "../../data-source";
-import { Client } from "../../entities/client.entity";
-import { AppError } from "../../errors/appError";
+import { Contact } from "../../entities/contact.entity";
 
-const listContactService = async (id: string) => {
-  try {
-    const clientsRepository = AppDataSource.getRepository(Client);
+const listContactService = async () => {
+  const contactRepository = AppDataSource.getRepository(Contact);
 
-    const clients = await clientsRepository.findOne({
-      where: {
-        id,
-      },
-      relations: {
-        contact: true,
-      },
-    });
-    if (!clients) {
-      throw new AppError("Client not found", 404);
-    }
-  } catch (error) {
-    throw new AppError("ID Invalid", 404);
-  }
+  const contacts = contactRepository.find({
+    relations: { client: true },
+  });
+
+  return contacts;
 };
 
 export default listContactService;
