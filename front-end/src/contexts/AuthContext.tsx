d0b71e2ea81c9703d279097dddb8ee8dcdd87f9f
richzago <rich.czago@gmail.com>
2023-02-04 16:34:48 -0300

    feat: brought the dashboard componen

--- front-end/src/contexts/AuthContext.tsx
+++ front-end/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext, ReactNode, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 import { toast } from "react-hot-toast";
+import { IClientsList } from "../interfaces/client";
 
 export const AuthContext = createContext<clientContextData>(
   {} as clientContextData
@@ -26,17 +27,21 @@ interface IClientProps {
 interface clientContextData {
   clientRegister: (IDataRegister: IDataRegister) => void;
   clientLogin: (IDataClientLogin: IDataClientLogin) => void;
+  listClients: (
+    IClientsList: IClientsList | React.MouseEvent<HTMLButtonElement>
+  ) => void;
+  deleteClient: (id: string) => void;
+  clients: IClientsList[];
 }
 const AuthProvider = ({ children }: IClientProps) => {
   const clientUser = localStorage.getItem("clientObject");
   const [client, setClient] = useState(JSON.parse(clientUser!));
+  const [clients, setClients] = useState<IClientsList[]>([]);
   const navigate = useNavigate();
 
   const clientRegister = (data: IDataRegister) => {
-    console.log("clientRegister", data);
     const postAPI = async () => {
       return api.post("/clients", data).then((response) => {
-        console.log("response", response);
         response.status === 201 && navigate("/login");
         return response;
       });
@@ -62,11 +67,71 @@ const AuthProvider = ({ children }: IClientProps) => {
       });
   };
 
+  const listClients = async () => {
+    try {
+      const token = localStorage.getItem("@TOKEN");
+      if (token) {
+        const response = await api.get("clients", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setClients(response.data);
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      localStorage.removeItem("@TOKEN");
+      console.error(error);
+    }
+  };
+
+  const deleteClient = async (id: string) => {
+    try {
+      const token = localStorage.getItem("@TOKEN");
+
+      if (token) {
+        const response = await api.delete(`/clients/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.status === 204) {
+          setClients(clients.filter((client) => client.id !== id));
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateClient = async (id: string) => {
+    try {
+      const token = localStorage.getItem("@TOKEN");
+
+      if (token) {
+        const response = await api.patch(`/clients/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.status === 200) {
+          console.log(response);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <AuthContext.Provider
       value={{
         clientRegister,
         clientLogin,
+        listClients,
+        clients,
+        deleteClient,
       }}
     >
       {children}
