964e559962c4159e5e0b566f5395ebc987643032
richzago <rich.czago@gmail.com>
2023-02-05 09:33:55 -0300

    feat: added retrieve client

--- back-end/src/services/clients/listClient.service.ts
+++ back-end/src/services/clients/listClient.service.ts
@@ -1,13 +1,14 @@
 import { AppDataSource } from "../../data-source";
 import { Client } from "../../entities/client.entity";
 
-const listClientsService = async(): Promise<Client[]> => {
-
-    const clientRepository = AppDataSource.getRepository(Client)
-
-    const clients = clientRepository.find({relations: {contact: true}})
-
+const listClientsService = async (id: string): Promise<Client[]> => {
+    const clientRepository = AppDataSource.getRepository(Client);
+  
+    const clients = clientRepository.find({
+      where: { id: id },
+    });
+  
     return clients;
-
-}
-export default listClientsService;
+  };
+  export default listClientsService;
+  
