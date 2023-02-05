1c96c7d31d52ed2243b41fae363c46f5cb16cdf0
richzago <rich.czago@gmail.com>
2023-02-04 23:39:01 -0300

    feat: added new style

--- front-end/src/components/dashboard/client/style.ts
+++ front-end/src/components/dashboard/client/style.ts
@@ -5,6 +5,11 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  p,
+  h3 {
+    color: white;
+  }
 `;
 
 export const MainDiv = styled.div`
@@ -12,4 +17,27 @@ export const MainDiv = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  button {
+    background-color: rgba(23, 26, 32, 0.8);
+    border: none;
+    border-radius: 6px;
+    color: #fff;
+    flex-grow: 1;
+    font-family: SFProText-Regular, Helvetica, Arial, sans-serif;
+    font-size: 15px;
+    height: 36px;
+    line-height: 20px;
+    margin-left: 8px;
+    margin-right: 6px;
+    min-width: 185px;
+    padding: 0 16px 0 16px;
+    color: white;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: rgba(23, 26, 32, 1);
+    cursor: pointer;
+  }
 `;
