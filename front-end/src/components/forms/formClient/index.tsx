44011f26546e1deb7d21443e9b614311b402095a
richzago <rich.czago@gmail.com>
2023-02-05 18:09:53 -0300

    feat: ajust form client

--- front-end/src/components/forms/formClient/index.tsx
+++ front-end/src/components/forms/formClient/index.tsx
@@ -2,6 +2,8 @@ import { useForm } from "react-hook-form";
 import { Container, FormStyle } from "./style";
 import { AuthContext, IDataRegister } from "../../../contexts/AuthContext";
 import { useContext } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schemaClientRegister } from "../../../validations/validationRegister";
 
 export const FormClientRegister = () => {
   const { clientRegister } = useContext(AuthContext);
@@ -10,10 +12,13 @@ export const FormClientRegister = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IDataRegister>({});
+  } = useForm<IDataRegister>({
+    resolver: yupResolver(schemaClientRegister),
+  });
 
   const onSubmitFunction = (data: IDataRegister) => {
     const castedData = data as IDataRegister;
+    console.log(data);
     clientRegister(castedData);
   };
 
@@ -22,20 +27,25 @@ export const FormClientRegister = () => {
       <FormStyle onSubmit={handleSubmit(onSubmitFunction)}>
         <h2>Make your registration</h2>
         <input type="text" placeholder="Full Name" {...register("name")} />
-        <p>{errors.name?.message}</p>
+        <p className="error">{errors.name?.message}</p>
 
-        <input type="text" placeholder="Email" {...register("email")} />
+        <input
+          className="error"
+          type="text"
+          placeholder="Email"
+          {...register("email")}
+        />
         <p>{errors.email?.message}</p>
+
+        <input type="text" placeholder="Phone" {...register("telephone")} />
+        <p className="error">{errors.telephone?.message}</p>
 
         <input
           type="password"
           placeholder="Password"
           {...register("password")}
         />
-        <p>{errors.password?.message}</p>
-
-        <input type="text" placeholder="Phone" {...register("telephone")} />
-        <p>{errors.telephone?.message}</p>
+        <p className="error">{errors.password?.message}</p>
 
         <button className="btn" type="submit">
           Register
