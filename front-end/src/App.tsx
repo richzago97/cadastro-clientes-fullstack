3cfbf16cdca658f71c900d77e7451410cfbe9773
richzago <rich.czago@gmail.com>
2023-02-05 18:06:55 -0300

    feat: added toast

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
 import { RoutesMain } from "./routes";
 
 function App() {
-  return <RoutesMain />;
+  return (
+    <>
+      <Toaster
+        toastOptions={{
+          style: {
+            border: "2px solid var(--gray-1)",
+          },
+        }}
+      />
+      <RoutesMain />
+    </>
+  );
 }
 
 export default App;
